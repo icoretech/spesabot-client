ba972d5f58f28f524bff910b1e2cf5180d3588c9
Claudio Poli <masterkain@gmail.com>
2020-05-06 22:06:07 +0200

    build

--- forge.config.js
+++ forge.config.js
@@ -24,7 +24,7 @@ const config = {
     },
     osxSign: {
       identity: process.env.OSX_SIGN_IDENTITY,
-      'hardened-runtime': true,
+      'hardenedRuntime': true,
       'gatekeeper-assess': false,
       'entitlements': 'static/entitlements.plist',
       'entitlements-inherit': 'static/entitlements.plist',

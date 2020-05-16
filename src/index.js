e4d624bb13404284d83ba84b3df7a36f110ea6ed
Claudio Poli <masterkain@gmail.com>
2020-05-16 16:05:26 +0200

    update channel

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const {connect, disconnect, isConnected} = require('./messenger');
 try {
   const updateServer = 'https://download.spesabot.com';
   const updateSuffix = process.platform === 'darwin' ? `/RELEASES.json?method=JSON&version=${app.getVersion()}` : '';
-  const updateFeed = `${updateServer}/spesabot/5bc3702a82f36211a0bd4d5e431f74ca/${process.platform}/${process.arch}${updateSuffix}`
+  const updateFeed = `${updateServer}/spesabot/0b1fdcf9e30320cf383f8b1e7f488969/${process.platform}/${process.arch}${updateSuffix}`
   autoUpdater.setFeedURL({
     url: updateFeed,
     serverType: 'json'
